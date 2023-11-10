642100c52f0156a47073e9c10458c9ca569ad3a4
Utkarsh Pandey <utk2665@gmail.com>
2023-11-10 18:10:06 +0530

    Site Finalized. Keypoints - Reusabilty

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import { footerLinks, socialMedia } from "../constants";
+import { logo } from "../assets";
+const Footer = () => (
+  <section
+    id="footer"
+    className={`${styles.flexCenter} ${styles.paddingX} ${styles.paddingY} flex-col`}
+  >
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+      <div className="sm:pr-20 pr-8">
+        <img
+          src={logo}
+          alt="hoobank"
+          className="w-[266px] h-[72px] object-contain"
+        />
+        <p className={styles.paragraph}>
+          A new way to make payments <br className="sm:block hidden" />
+          easy, reliable and secure.
+        </p>
+      </div>
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+        {footerLinks.map((footerLink) => (
+          <div
+            key={footerLink.key}
+            className="flex flex-col ss:my-0 my-4 min-w-[150px]"
+          >
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+              {footerLink.title}
+            </h4>
+            <ul className="list-none mt-4">
+              {footerLink.links.map((link, index) => (
+                <li
+                  key={link.name}
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+                    index !== footerLink.links.length - 1 ? "mb-4" : "mb-0"
+                  }`}
+                >
+                  {link.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]">
+      <p className="font-poppins font-normal text-center text-[18px] text-white leading-[27px]">
+        2023 HooBank. All Rights Reserved.
+      </p>
 
-export default Footer
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+          <img
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+            }`}
+          />
+        ))}
+      </div>
+    </div>
+  </section>
+);
+
+export default Footer;
